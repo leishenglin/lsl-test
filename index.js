4cd4109464363ea8e5c5f91cc70540519f2e06e5
雷声林 <leisl@k-group.com.cn>
2024-05-31 12:54:04 +0800

    1

--- index.js
+++ index.js
@@ -5,7 +5,17 @@ import proxy from 'puppeteer-extra-plugin-proxy'
 import pluginSetalth from 'puppeteer-extra-plugin-stealth'
 import socks from 'socks'
 import sockpProxyAgent from 'socks-proxy-agent'
-import url from 'url'
+import ahooks from 'ahooks'
+import antdImgCrop from 'antd-img-crop'
+import autoprefixer from 'autoprefixer'
+import axios from 'axios'
+import postcss from 'postcss'
+import qrcode from 'qrcode'
+import smCrypto from 'sm-crypto'
+import tailwindcss from 'tailwindcss'
+import wangeditor from '@wangeditor/editor'
+import antv from '@antv/g2plot'
+import amap from '@amap/amap-jsapi-loader'
 import { generateRandomUserAgent } from './utils/utils'
 
 puppeteer.use(pluginSetalth())
@@ -15,6 +25,17 @@ console.log(proxy)
 console.log(socks)
 console.log(sockpProxyAgent)
 console.log(url)
+console.log(ahooks)
+console.log(antdImgCrop)
+console.log(autoprefixer)
+console.log(axios)
+console.log(postcss)
+console.log(qrcode)
+console.log(smCrypto)
+console.log(tailwindcss)
+console.log(wangeditor)
+console.log(antv)
+console.log(amap)
 
 const b = puppeteer.launch();
 console.log(b)
